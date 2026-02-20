8ee494c28268b2adc01f2f0f08d6f771a4208cb1
CrisGreenfield <cristobal.greenfield03@somo.eus>
2026-02-20 11:10:56 +0100

    Añadido firebase al proyecto

--- SmashRide/app/build.gradle.kts
+++ SmashRide/app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+    id("com.google.gms.google-services")
+    // alias(libs.plugins.android.application)
 }
 
 android {
@@ -32,7 +34,8 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
